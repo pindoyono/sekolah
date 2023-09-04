582e70d1cb10b7c7832d8571079aae99895589a3
pindoyono <pindoyonopatan@gmail.com>
2023-09-04 11:22:07 +0800

    user resource

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Pages\Page;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Illuminate\Support\Facades\Hash;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\UserResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                TextInput::make('name')
+                ->label('Nama')
+                ->required()
+                ->maxLength(100),
+
+                TextInput::make('email')
+                ->email()
+                ->label('Alamat Email')
+                ->required()
+                ->maxLength(100),
+
+                TextInput::make('password')
+                ->password()
+                ->minLength(8)
+                ->same('passwordConfirmation')
+                ->dehydrateStateUsing(fn (string $state):string => Hash::make($state))
+                ->dehydrated(fn($state):bool => filled($state))
+                ->required(fn(Page $livewire):bool => $livewire instanceof CreateRecord),
+
+                TextInput::make('passwordConfirmation')
+                ->password()
+                ->label('Komfirmasi Password')
+                ->required(fn(Page $livewire):bool => $livewire instanceof CreateRecord)
+                ->minLength(8)
+                ->dehydrated(false),
+
+
+                Select::make('roles')->multiple()->relationship('roles','name')
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('No')->rowIndex(),
+                TextColumn::make('name'),
+                TextColumn::make('roles.name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,21 +2,33 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
-use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Filament\PanelProvider;
+use Filament\Pages\Dashboard;
+use Filament\Navigation\MenuItem;
+use Filament\Support\Colors\Color;
+use App\Filament\Resources\GtkResource;
+use Filament\Navigation\NavigationItem;
+use App\Filament\Resources\UserResource;
+use Filament\Navigation\NavigationGroup;
+use App\Filament\Resources\SiswaResource;
+use App\Filament\Resources\RombelResource;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Navigation\NavigationBuilder;
+use App\Filament\Resources\SekolahResource;
+use Illuminate\Session\Middleware\StartSession;
+use App\Filament\Resources\PembelajaranResource;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -63,6 +75,55 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->viteTheme('resources/css/filament/admin/theme.css');
+            ->viteTheme('resources/css/filament/admin/theme.css')
+            ->plugin(FilamentSpatieRolesPermissionsPlugin::make())
+            ->navigation(function (NavigationBuilder $builder): NavigationBuilder {
+                return $builder->groups([
+                    NavigationGroup::make()
+                        ->items([
+                            NavigationItem::make('Dashboard')
+                            ->icon('heroicon-o-home')
+                            ->isActiveWhen(fn (): bool => request()->routeIs('filament.admin.pages.dashboard'))
+                            ->url(fn (): string => Dashboard::getUrl()),
+                        ]),
+                    NavigationGroup::make('Dapodik')
+                        ->items([
+                            ...GtkResource::getNavigationItems(),
+                            ...PembelajaranResource::getNavigationItems(),
+                            ...RombelResource::getNavigationItems(),
+                            ...SekolahResource::getNavigationItems(),
+                            ...SiswaResource::getNavigationItems(),
+                        ]),
+                    NavigationGroup::make('Setting')
+                        ->items([
+                            ...UserResource::getNavigationItems(),
+                            NavigationItem::make('Roles')
+                            ->visible(fn(): bool => auth()->user()->can('roles'))
+                            ->icon('heroicon-o-puzzle-piece')
+                            ->isActiveWhen(fn (): bool => request()->routeIs([
+                                    'filament.admin.resources.roles.index',
+                                    'filament.admin.resources.roles.create',
+                                    'filament.admin.resources.roles.view',
+                                    'filament.admin.resources.roles.edit',
+                                ]))
+                            ->url(fn (): string => '/admin/roles'),
+                            NavigationItem::make('Permissions')
+                            ->visible(fn(): bool => auth()->user()->can('permissions'))
+                            ->icon('heroicon-o-key')
+                            ->isActiveWhen(fn (): bool => request()->routeIs([
+                                    'filament.admin.resources.permissions.index',
+                                    'filament.admin.resources.permissions.create',
+                                    'filament.admin.resources.permissions.view',
+                                    'filament.admin.resources.permissions.edit',
+                                ]))
+                            ->url(fn (): string => '/admin/permissions'),
+                        ]),
+                ]);
+            });
+            // ->userMenuItems([
+            //     'logout' => MenuItem::make()->label('Log out'),
+            //     // ...
+            // ]);
+
     }
 }
